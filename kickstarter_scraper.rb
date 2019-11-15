2cfb67075b0e73bc8743563754b65e2a07e95e16
Randy Steele <steelerx155@gmail.com>
2019-11-15 00:19:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,9 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = {}
-  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
 end
  
 create_project_hash
